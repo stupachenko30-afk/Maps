ce066d795e429e7a5a81faac8bd8cea72efdd01c
Yevhenii <stupachenko30@gmail.com>
2026-06-08 12:41:45 +0200

    add

---
+++ src/main/java/MainMaps.java
@@ -0,0 +1,18 @@
+public class MainMaps {
+
+}
+
+
+/*
+
+Map
+         |            |
+   HashMap          TreeMap
+       |
+ LinkedHashMap
+      keys========================================
+set ["kiwi", "apple", "grape", "orange", "banana"]
+       |        |       |         |          |
+      50        70     20        80         70
+      value======================================
+ */
